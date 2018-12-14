8e772fb05876820468c3330e957ae41aea1196f9
Joseph Mirabel <jmirabel@laas.fr>
2018-12-14 18:25:11 +0100

    Make root joint type parameterizable.

--- src/hpp/corbaserver/ur5/robot.py
+++ src/hpp/corbaserver/ur5/robot.py
@@ -24,8 +24,8 @@ class Robot (Parent):
     urdfSuffix = ""
     srdfSuffix = ""
 
-    def __init__ (self, robotName, load = True):
-        Parent.__init__ (self, robotName, "anchor", load)
+    def __init__ (self, robotName, load = True, rootJointType = "anchor"):
+        Parent.__init__ (self, robotName, rootJointType, load)
         self.rightWrist = "wrist_3_joint"
         self.leftWrist  = "wrist_3_joint"
         self.endEffector = "ee_fixed_joint"
